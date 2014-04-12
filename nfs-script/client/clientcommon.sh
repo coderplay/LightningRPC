3c2f5d67b156b2b3b901787736794e780e6c80e3
Min Zhou <coderplay@gmail.com>
2014-04-12 00:30:19 -0700

    Add comment on data types

--- nfs-script/client/clientcommon.sh
+++ nfs-script/client/clientcommon.sh
@@ -1,1 +1,6 @@
+# datatype
+#  java     1
+#  hessian  2
+#  protobuf 3
+#  kryo     4
 java -Xms4g -Xmx4g -Xmn1g -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:gc.log -Dwrite.statistics=true -Djava.ext.dirs="../../lib" $1 [serverip] [serverport] [concurrents] [timeout] [datatype] [requestsize] [runtime] [clientnums] > $2 2>&1 &
