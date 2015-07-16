3ee999ddb1ed8db94eebb2a2531b6bf318e2e361
Min Zhou <coderplay@gmail.com>
2015-07-16 11:24:46 -0700

    Reformat some classes

--- lightningrpc-common/src/main/java/info/minzhou/lightning/rpc/server/SimpleProcessorServerHandler.java
+++ lightningrpc-common/src/main/java/info/minzhou/lightning/rpc/server/SimpleProcessorServerHandler.java
@@ -9,34 +9,33 @@ import org.apache.commons.logging.LogFactory;
 
 import info.minzhou.lightning.rpc.RequestWrapper;
 import info.minzhou.lightning.rpc.ResponseWrapper;
+
 /**
  * Direct Call RPC Server Handler
- * 
  */
-public class SimpleProcessorServerHandler implements ServerHandler{
+public class SimpleProcessorServerHandler implements ServerHandler {
 
-	private static final Log LOGGER = LogFactory.getLog(SimpleProcessorServerHandler.class);
-	
-	private Map<String, ServerProcessor> processors = new ConcurrentHashMap<String, ServerProcessor>();
-	
-	public void registerProcessor(String instanceName,Object instance){
-		processors.put(instanceName, (ServerProcessor)instance);
-	}
-	
-	public ResponseWrapper handleRequest(final RequestWrapper request){
-		ResponseWrapper responseWrapper = new ResponseWrapper(request.getId(),request.getCodecType(),request.getProtocolType());
-		try{
-			String argType = null;
-			if(request.getArgTypes() != null && request.getArgTypes()[0] != null){
-				argType = new String(request.getArgTypes()[0]);
-			}
-			Object requestObject = Codecs.getDecoder(request.getCodecType()).decode(argType,(byte[])request.getMessage());
-			responseWrapper.setResponse(processors.get(requestObject.getClass().getName()).handle(requestObject));
-		}
-		catch(Exception e){
-			LOGGER.error("server direct call handler to handle request error",e);
-			responseWrapper.setException(e);
-		}
-		return responseWrapper;
-	}
+  private static final Log LOGGER = LogFactory.getLog(SimpleProcessorServerHandler.class);
+
+  private Map<String, ServerProcessor> processors = new ConcurrentHashMap<String, ServerProcessor>();
+
+  public void registerProcessor(String instanceName, Object instance) {
+    processors.put(instanceName, (ServerProcessor) instance);
+  }
+
+  public ResponseWrapper handleRequest(final RequestWrapper request) {
+    ResponseWrapper responseWrapper = new ResponseWrapper(request.getId(), request.getCodecType(), request.getProtocolType());
+    try {
+      String argType = null;
+      if (request.getArgTypes() != null && request.getArgTypes()[0] != null) {
+        argType = new String(request.getArgTypes()[0]);
+      }
+      Object requestObject = Codecs.getDecoder(request.getCodecType()).decode(argType, (byte[]) request.getMessage());
+      responseWrapper.setResponse(processors.get(requestObject.getClass().getName()).handle(requestObject));
+    } catch (Exception e) {
+      LOGGER.error("server direct call handler to handle request error", e);
+      responseWrapper.setException(e);
+    }
+    return responseWrapper;
+  }
 }
